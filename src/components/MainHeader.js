e26e88f6801369df6bc10164a9d24c9eb85b9954
Yago Santos de Sousa <yagosansz@gmail.com>
2020-03-16 20:12:55 -0400

    Initial config

---
+++ src/components/MainHeader.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+class MainHeader extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello, Header</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default MainHeader;
